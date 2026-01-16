262f7334726949794e8135a8d2ce3968c5a86fc6
Gunabh Sharan <gunabhsharan.25@gmail.com>
2026-01-16 06:25:09 -0600

    fixed printer file

--- hardware-service/src/devices/printer/printer.usb.js
+++ hardware-service/src/devices/printer/printer.usb.js
@@ -1,19 +1,25 @@
 import ThermalPrinter from 'node-thermal-printer';
+import logger from '../../utils/logger.js';
 
 const printer = new ThermalPrinter.printer({
   type: ThermalPrinter.types.EPSON,
   interface: 'usb'
 });
 
-export async function printReceipt(lines) {
-  printer.alignCenter();
-  printer.println('SHOP NAME');
-  printer.drawLine();
+export async function printReceipt(lines = []) {
+  try {
+    printer.alignCenter();
+    printer.println('POS RECEIPT');
+    printer.drawLine();
 
-  lines.forEach(line => printer.println(line));
+    lines.forEach(l => printer.println(l));
 
-  printer.drawLine();
-  printer.cut();
+    printer.drawLine();
+    printer.cut();
+    await printer.execute();
 
-  await printer.execute();
+    logger.info('Receipt printed');
+  } catch (err) {
+    logger.error(`Printer error: ${err.message}`);
+  }
 }
